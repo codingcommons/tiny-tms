8432f693c294d57197e54df9b7f0280a5e6e0b9e
Benjamin Strasser <bp.strasser@gmail.com>
2024-04-29 18:46:41 +0200

    Added database setup
    Signed-off-by: Benjamin Strasser <bp.strasser@gmail.com>

---
+++ packages/service/src/db/database-migration-util.ts
@@ -0,0 +1,15 @@
+import { migrate, undoMigration } from '../kysely/migrator-util';
+
+export async function runMigration() {
+  const { error } = await migrate('migrations/js');
+  if (error) {
+    throw new Error('Database Migration failed');
+  }
+}
+
+export async function runUndoMigration() {
+  const { error } = await undoMigration('migrations/js');
+  if (error) {
+    throw new Error('Undoing Database Migration failed');
+  }
+}
