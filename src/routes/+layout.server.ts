4ea40a8d3cf9694907349632bb59350712d5cdeb
Sasan Jaghori <sasan.jaghori@iteratec.com>
2024-05-02 23:35:35 +0200

    remove import

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,9 +1,5 @@
-import { getUserFromJWT } from 'services/auth/auth'
 import type { LayoutServerLoad } from './$types'
 
 export const load: LayoutServerLoad = async () => {
-	// TEST
-	await getUserFromJWT('TEST')
-
 	return {}
 }
