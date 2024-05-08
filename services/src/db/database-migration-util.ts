34766531ddce0c27a3a3eed8bc337d28fa5721bc
Benjamin Strasser <bp.strasser@gmail.com>
2024-05-08 15:28:38 +0200

    added custom vite esm kysely migrator to run inside integration tests
    Signed-off-by: Benjamin Strasser <bp.strasser@gmail.com>

--- services/src/db/database-migration-util.ts
+++ services/src/db/database-migration-util.ts
@@ -1,14 +1,14 @@
 import { migrate, undoMigration } from '../kysely/migrator-util'
 
 export async function runMigration() {
-	const { error } = await migrate('migrations/js')
+	const { error } = await migrate({ providerType: 'VITE_ESM_FILE' })
 	if (error) {
 		throw new Error('Database Migration failed')
 	}
 }
 
 export async function runUndoMigration() {
-	const { error } = await undoMigration('migrations/js')
+	const { error } = await undoMigration({ providerType: 'VITE_ESM_FILE' })
 	if (error) {
 		throw new Error('Undoing Database Migration failed')
 	}
--- services/src/kysely/migrator-util.ts
+++ services/src/kysely/migrator-util.ts
@@ -1,32 +1,76 @@
 import * as path from 'node:path'
 import * as url from 'node:url'
 import { promises as fs } from 'node:fs'
-import { FileMigrationProvider, Migrator, NO_MIGRATIONS } from 'kysely'
+import {
+	FileMigrationProvider,
+	type Migration,
+	type MigrationProvider,
+	Migrator,
+	NO_MIGRATIONS
+} from 'kysely'
 import { db } from '../db/database'
 
-export async function migrate(migrationsLocation = 'migrations') {
-	const migrator = getMigrator(migrationsLocation)
+type ProviderType = 'FILE' | 'VITE_ESM_FILE'
+
+export type MigrationConfig = {
+	providerType: ProviderType
+}
+
+const defaultConfig: MigrationConfig = {
+	providerType: 'FILE'
+}
+
+export async function migrate(migrationConfig: Partial<MigrationConfig> = defaultConfig) {
+	const config = {
+		...defaultConfig,
+		...migrationConfig
+	}
+
+	const migrator = getMigrator(config)
 
 	return migrator.migrateToLatest()
 }
 
-export async function undoMigration(migrationsLocation = 'migrations') {
-	const migrator = getMigrator(migrationsLocation)
+export async function undoMigration(migrationConfig: Partial<MigrationConfig> = defaultConfig) {
+	const config = {
+		...defaultConfig,
+		...migrationConfig
+	}
+	const migrator = getMigrator(config)
 
 	return migrator.migrateTo(NO_MIGRATIONS)
 }
 
-function getMigrator(migrationsLocation: string) {
-	const __dirname = url.fileURLToPath(new URL('.', import.meta.url))
-	const provider = new FileMigrationProvider({
-		fs,
-		path,
-		// This needs to be an absolute path.
-		migrationFolder: path.join(__dirname, migrationsLocation)
-	})
+function getMigrator(config: MigrationConfig) {
+	const provider = getProvider(config)
 
 	return new Migrator({
 		db,
 		provider
 	})
+}
+
+function getProvider(config: MigrationConfig): MigrationProvider {
+	const providerType = config.providerType
+
+	switch (providerType) {
+		case 'FILE':
+			return new FileMigrationProvider({
+				fs,
+				path,
+				migrationFolder: path.join(url.fileURLToPath(new URL('.', import.meta.url)), 'migrations')
+			})
+		case 'VITE_ESM_FILE':
+			return new ViteFileMigrationProvider()
+	}
+}
+
+class ViteFileMigrationProvider implements MigrationProvider {
+	async getMigrations(): Promise<Record<string, Migration>> {
+		const migrations: Record<string, Migration> = import.meta.glob('./migrations/**.ts', {
+			eager: true
+		})
+
+		return migrations
+	}
 }
