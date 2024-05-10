c6f4c3c7e0ed548976c882a91f0a85f6a785da0b
Benjamin Strasser <bp.strasser@gmail.com>
2024-05-10 14:15:58 +0200

    Added Dashboard
    Signed-off-by: Benjamin Strasser <bp.strasser@gmail.com>

---
+++ src/routes/dashboard/(components)/ProjectCard.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import * as Card from '$components/ui/card/index.js'
+
+	export let project: { id: number; name: string; fallback_language?: number }
+</script>
+
+<Card.Root>
+	<Card.Header>
+		<Card.Title>{project.name}</Card.Title>
+		<Card.Description>Add a new payment method to your account.</Card.Description>
+	</Card.Header>
+	<Card.Content class="grid gap-6">Content</Card.Content>
+</Card.Root>
---
+++ src/routes/dashboard/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import ProjectCard from './(components)/ProjectCard.svelte'
+	const projects = [
+		{ id: 0, name: 'Tiny-TMS', fallback_language: 1 },
+		{ id: 1, name: 'Super Cool Project 2', fallback_language: 1 },
+		{ id: 2, name: 'Google', fallback_language: 0 },
+		{ id: 3, name: 'i18n', fallback_language: 0 },
+		{ id: 4, name: 'Other Stuff Project', fallback_language: 0 }
+	]
+</script>
+
+<div
+	class="hidden items-start justify-center gap-6 rounded-lg p-8 md:grid lg:grid-cols-2 xl:grid-cols-3"
+>
+	{#each projects as project}
+		<ProjectCard {project} />
+	{/each}
+</div>
