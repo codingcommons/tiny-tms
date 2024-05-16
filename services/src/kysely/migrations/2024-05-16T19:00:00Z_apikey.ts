be590f91836b5b59704eae81278ab32c072d2064
Jacob Palecek <jacob.palecek@outlook.com>
2024-05-16 19:27:39 +0200

    add migration

---
+++ services/src/kysely/migrations/2024-05-16T19:00:00Z_apikey.ts
@@ -0,0 +1,15 @@
+import { Kysely } from 'kysely'
+import { createTableMigration } from '../migration.util'
+
+export async function up(db: Kysely<unknown>): Promise<void> {
+	await createTableMigration(db, 'apikeys')
+		.addColumn('key', 'text', (col) => col.unique().notNull())
+		.addColumn('project_id', 'integer', (col) =>
+			col.references('projects.id').onDelete('cascade').notNull()
+		)
+		.execute()
+}
+
+export async function down(db: Kysely<unknown>): Promise<void> {
+	await db.schema.dropTable('apikeys').execute()
+}
