b5804ebb072a842c24cbc4e3c498f5f370530d46
Sasan Jaghori <sasan.jaghori@iteratec.com>
2024-05-31 21:15:24 +0200

    adds docs template setup

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+	title: 'Tiny TMS',
+	description:
+		'A Translation Management System to cover the needs of most people. Not for everyone, but for most.',
+	themeConfig: {
+		// https://vitepress.dev/reference/default-theme-config
+		nav: [
+			{ text: 'Home', link: '/' },
+			{ text: 'Examples', link: '/markdown-examples' }
+		],
+
+		sidebar: [
+			{
+				text: 'Examples',
+				items: [
+					{ text: 'Markdown Examples', link: '/markdown-examples' },
+					{ text: 'Runtime API Examples', link: '/api-examples' }
+				]
+			}
+		],
+
+		socialLinks: [{ icon: 'github', link: 'https://github.com/vuejs/vitepress' }]
+	}
+})
---
+++ docs/index.md
@@ -0,0 +1,24 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  name: 'Tiny TMS'
+  text: 'A Translation Management System to cover the needs of most people. Not for everyone, but for most.'
+  tagline: My great project tagline
+  actions:
+    - theme: brand
+      text: Markdown Examples
+      link: /markdown-examples
+    - theme: alt
+      text: API Examples
+      link: /api-examples
+
+features:
+  - title: Feature A
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+  - title: Feature B
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+  - title: Feature C
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+---
