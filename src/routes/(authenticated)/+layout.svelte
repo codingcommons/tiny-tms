f3b246197264ce36f8519a5926534adcd96e0ef2
Benjamin Strasser <bp.strasser@gmail.com>
2024-06-01 09:20:45 +0200

    Sidebar is collapsable
    Signed-off-by: Benjamin Strasser <bp.strasser@gmail.com>

--- src/routes/(authenticated)/+layout.svelte
+++ src/routes/(authenticated)/+layout.svelte
@@ -1,39 +1,69 @@
 <script lang="ts">
 	import { setContext } from 'svelte'
-	import { type Writable, writable } from 'svelte/store'
+	import { writable } from 'svelte/store'
 	import type { NavigationElement } from './sidebar-context'
 	import { page } from '$app/stores'
 	import { ThemeSelector } from '$components/ui/theme-selector'
 
-	const sidebarElements: Writable<NavigationElement[]> = writable([])
+	import ChevronLeft from 'lucide-svelte/icons/chevron-left'
+	import ChevronRight from 'lucide-svelte/icons/chevron-right'
 
+	const sidebarElements = writable<NavigationElement[]>([])
 	setContext('sidebar', sidebarElements)
+
+	let collapsedSidebar = false
 </script>
 
 <div class="flex h-full">
-	<div class="flex h-full w-80 flex-none flex-col bg-secondary text-secondary-foreground">
+	<div
+		class:collapsedSidebar
+		class="sidebar duration-400 flex h-full w-80 flex-none flex-col bg-secondary text-secondary-foreground transition-all ease-in-out"
+	>
 		<!-- Top Section -->
-		<div class="m-8 mb-6 text-2xl">
-			<span>⌘ Tiny-TMS</span>
+		<div class="m-8 mb-6 mr-0 flex items-center text-2xl">
+			<div>⌘</div>
+			<div class="ml-2 overflow-hidden whitespace-nowrap" class:collapsedSidebar>Tiny-TMS</div>
+			<button
+				on:click={() => (collapsedSidebar = !collapsedSidebar)}
+				class="ml-auto mr-2 cursor-pointer rounded-full hover:bg-primary-foreground"
+			>
+				{#if collapsedSidebar}
+					<ChevronRight />
+				{:else}
+					<ChevronLeft />
+				{/if}
+			</button>
 		</div>
 
 		<div class="mx-4 flex-grow">
 			{#each $sidebarElements as element}
 				<a
 					href={element.route}
-					class="flex cursor-pointer rounded-lg border border-transparent p-4 transition duration-100 ease-in-out hover:border-primary"
+					class="flex cursor-pointer rounded-lg border border-transparent p-4 hover:border-primary"
 					class:selected={$page.url.pathname.endsWith(element.route)}
 				>
-					<svelte:component this={element.icon} class="mr-2"></svelte:component>
-					<div>{element.name}</div>
+					<svelte:component this={element.icon} class="min-h-6 min-w-6"></svelte:component>
+					<div
+						class="ml-2 overflow-hidden whitespace-nowrap"
+						class:collapsedElement={collapsedSidebar}
+					>
+						{element.name}
+					</div>
 				</a>
 			{/each}
 		</div>
 
 		<div class="m-4 mt-auto flex items-center">
-			<div class="mr-2.5 h-10 w-10 rounded-full bg-yellow-300"></div>
-			<div class="mr-auto">My Account</div>
-			<ThemeSelector />
+			<div class="ml-4 mr-2.5 min-h-8 min-w-8 rounded-full bg-yellow-300"></div>
+			<div
+				class:collapsedElement={collapsedSidebar}
+				class="mr-auto overflow-hidden whitespace-nowrap"
+			>
+				My Account
+			</div>
+			<div class:collapsedElement={collapsedSidebar} class="overflow-hidden">
+				<ThemeSelector />
+			</div>
 		</div>
 	</div>
 
@@ -41,6 +71,13 @@
 </div>
 
 <style>
+	.collapsedElement {
+		display: none;
+	}
+
+	.collapsedSidebar {
+		@apply w-[90px];
+	}
 	.selected {
 		@apply bg-primary-foreground;
 	}
