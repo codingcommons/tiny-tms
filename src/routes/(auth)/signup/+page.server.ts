d9e4ba0ed293b5e413d7c110ed279f140a733656
Benjamin Strasser <bp.strasser@gmail.com>
2024-06-03 21:34:25 +0200

    redirect signed in users to /projects + after sign in
    Signed-off-by: Benjamin Strasser <bp.strasser@gmail.com>

--- src/routes/(auth)/signup/+page.server.ts
+++ src/routes/(auth)/signup/+page.server.ts
@@ -7,7 +7,7 @@ import { redirect } from '@sveltejs/kit'
 
 export const load: PageServerLoad = async (event) => {
 	if (event.locals.user) {
-		throw redirect(302, '/')
+		throw redirect(302, '/projects')
 	}
 
 	return {
