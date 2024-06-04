a545890d8d6eef874c7817a1a3e58bdf4d4fc47b
Sasan Jaghori <jaghori.sasan@gmail.com>
2024-06-04 17:32:23 +0200

    (feat) adds docs template (#76)
    * adds docs template setup
    
    * include vitepress config in tsconfig.json
    
    * change ci to trigger docs deploy
    
    * add base path
    
    * correct base path
    
    * remove on pull request docs deploy
    
    * unbumped versions
    
    Signed-off-by: Benjamin Strasser <bp.strasser@gmail.com>
    
    ---------
    
    Signed-off-by: Benjamin Strasser <bp.strasser@gmail.com>
    Co-authored-by: Sasan Jaghori <sasan.jaghori@iteratec.com>
    Co-authored-by: Benjamin Strasser <bp.strasser@gmail.com>

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,28 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+	base: '/tiny-tms/',
+	title: 'Tiny TMS',
+	description:
+		'A Translation Management System to cover the needs of most people. Not for everyone, but for most.',
+	themeConfig: {
+		// https://vitepress.dev/reference/default-theme-config
+		nav: [
+			{ text: 'Home', link: '/' },
+			{ text: 'Examples', link: '/markdown-examples' }
+		],
+
+		sidebar: [
+			{
+				text: 'Examples',
+				items: [
+					{ text: 'Markdown Examples', link: '/markdown-examples' },
+					{ text: 'Runtime API Examples', link: '/api-examples' }
+				]
+			}
+		],
+
+		socialLinks: [{ icon: 'github', link: 'https://github.com/codingcommons/tiny-tms' }]
+	}
+})
