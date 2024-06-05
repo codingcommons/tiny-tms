29251b3fcd46ce3851296df22737187fb6736093
Benjamin Strasser <bp.strasser@gmail.com>
2024-06-05 12:14:02 +0200

    added basic navigation
    Signed-off-by: Benjamin Strasser <bp.strasser@gmail.com>

--- src/routes/(authenticated)/projects/project-cards.svelte
+++ src/routes/(authenticated)/projects/project-cards.svelte
@@ -34,20 +34,22 @@
 </script>
 
 <div class="grid grid-cols-1 gap-2 xl:grid-cols-2 2xl:grid-cols-3">
-	{#each projects as project}
-		<Card.Root class="min-w-80 hover:bg-primary-foreground">
-			<Card.Header class="h-20">
-				<Card.Title class="text-2xl font-semibold">{project.name}</Card.Title>
-			</Card.Header>
-			<Card.Content class="flex h-28 items-end -space-x-2 pb-2">
-				<div class="h-10 w-10 rounded-full bg-yellow-400"></div>
-				<div class="h-10 w-10 rounded-full bg-red-400"></div>
-				<div class="h-10 w-10 rounded-full bg-blue-400"></div>
-				<div class="h-10 w-10 rounded-full bg-green-400"></div>
-			</Card.Content>
-			<Card.CardFooter class="h-14 pb-1 text-sm text-muted-foreground">
-				Last updated: {project.lastUpdated}
-			</Card.CardFooter>
-		</Card.Root>
+	{#each projects as project, index}
+		<a href={`/projects/${index}/translations`}>
+			<Card.Root class="min-w-80 cursor-pointer hover:bg-primary-foreground">
+				<Card.Header class="h-20">
+					<Card.Title class="text-2xl font-semibold">{project.name}</Card.Title>
+				</Card.Header>
+				<Card.Content class="flex h-28 items-end -space-x-2 pb-2">
+					<div class="h-10 w-10 rounded-full bg-yellow-400"></div>
+					<div class="h-10 w-10 rounded-full bg-red-400"></div>
+					<div class="h-10 w-10 rounded-full bg-blue-400"></div>
+					<div class="h-10 w-10 rounded-full bg-green-400"></div>
+				</Card.Content>
+				<Card.CardFooter class="h-14 pb-1 text-sm text-muted-foreground">
+					Last updated: {project.lastUpdated}
+				</Card.CardFooter>
+			</Card.Root>
+		</a>
 	{/each}
 </div>
