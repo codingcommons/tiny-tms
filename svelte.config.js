8054515b9181f69e7dd75a55a7d5efde0fda90c2
Sasan Jaghori <jaghori.sasan@gmail.com>
2024-06-27 21:52:21 +0200

    adds vercel adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,9 @@
-import adapter from '@sveltejs/adapter-node'
+import nodeAdapter from '@sveltejs/adapter-node'
+import vercelAdapter from '@sveltejs/adapter-vercel'
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+import { config as envConfig } from 'dotenv'
+
+envConfig()
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -8,10 +12,7 @@ const config = {
 	preprocess: [vitePreprocess({})],
 
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter(),
+		adapter: process.env.VERCEL ? vercelAdapter() : nodeAdapter(),
 		alias: {
 			$components: 'src/components',
 			$models: 'src/models',
