e7f77c26de28b3cb4bc2ec1df98ba6db121cd89b
Sasan Jaghori <jaghori.sasan@gmail.com>
2024-06-27 21:35:08 +0200

    adds cloudflare adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,9 @@
-import adapter from '@sveltejs/adapter-node'
+import nodeAdapter from '@sveltejs/adapter-node'
+import cloudflareAdapter from '@sveltejs/adapter-cloudflare'
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+import * as env from 'dotenv'
+
+env.config()
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -8,10 +12,7 @@ const config = {
 	preprocess: [vitePreprocess({})],
 
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter(),
+		adapter: process.env.CF_PAGES ? cloudflareAdapter() : nodeAdapter(),
 		alias: {
 			$components: 'src/components',
 			$models: 'src/models',
