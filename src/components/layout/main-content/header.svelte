80b0baa13422c31f1105c405a5c694fe0756d2c2
mledl <martin.ledl55@gmail.com>
2024-08-21 17:08:37 +0200

    adjusts main content and header component styles

--- src/components/layout/main-content/header.svelte
+++ src/components/layout/main-content/header.svelte
@@ -3,6 +3,9 @@
 </script>
 
 <div class="mb-28 flex items-center justify-between">
-	<h2 class="border-b-2 text-3xl font-semibold">{title}</h2>
+	<div class="flex w-full justify-between">
+		<h2 class="border-b-2 text-3xl font-semibold">{title}</h2>
+		<slot name="actions" />
+	</div>
 	<slot />
 </div>
--- src/components/layout/main-content/main-content.svelte
+++ src/components/layout/main-content/main-content.svelte
@@ -1,3 +1,11 @@
-<div class="h-full w-full overflow-x-auto px-16 pt-8">
+<script lang="ts">
+	import { cn } from '$utils'
+	import type { HTMLAttributes } from 'svelte/elements'
+	type $$Props = HTMLAttributes<HTMLDivElement>
+	let className: $$Props['class'] = undefined
+	export { className as class }
+</script>
+
+<div class={cn('h-full w-full overflow-x-auto px-16 pt-8', className)} {...$$restProps}>
 	<slot />
 </div>
