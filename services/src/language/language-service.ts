2d14468991a399b34ebdedf6a4b502cf5b8596c4
mledl <martin.ledl55@gmail.com>
2024-08-22 11:03:30 +0200

    refactor: simplify getLanguagesForProject function and update return type

--- services/src/language/language-service.ts
+++ services/src/language/language-service.ts
@@ -1,18 +1,14 @@
 import type { LanguageCode } from '$components/container/language/languages'
-import type { LanguagesSchema } from '$components/container/language/schema'
+import type { LanguageSchema } from '$components/container/language/schema'
 import * as repository from './language-repository'
 
-export async function getLanguagesForProject(id: number): Promise<LanguagesSchema> {
-	console.warn(id)
+export async function getLanguagesForProject(id: number): Promise<LanguageSchema[]> {
 	const languages = await repository.getLanguagesForProject(id)
 
-	console.warn(languages)
-
-	return {
-		languages: languages.map(({ code, label, fallback_language }) => ({
-			code: code as LanguageCode,
-			label,
-			fallback: fallback_language as LanguageCode
-		}))
-	}
+	return languages.map(({ id, code, label, fallback_language }) => ({
+		id,
+		code: code as LanguageCode,
+		label,
+		fallback: fallback_language as LanguageCode
+	}))
 }
