e54256a38d37ff3c5fa4b1e395bc375d7eae3e0d
mledl <martin.ledl55@gmail.com>
2024-08-22 11:03:34 +0200

    feat: add updateLanguage function to language repository

--- services/src/language/language-repository.ts
+++ services/src/language/language-repository.ts
@@ -8,4 +8,16 @@ export function getLanguagesForProject(id: number): Promise<SelectableLanguage[]
 		.where('l1.project_id', '=', id)
 		.select(['l1.id', 'l1.code', 'l1.label', 'l2.code as fallback_language'])
 		.execute()
+
+export function updateLanguage(language: LanguageSchema): Promise<void> {
+	return db
+		.updateTable('languages')
+		.set({
+			code: language.code,
+			label: language.label,
+			fallback_language: language.fallback || null
+		})
+		.where('id', '=', language.id!)
+		.execute()
+}
 }
