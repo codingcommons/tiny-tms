98b4cdede994174a602bbf9b5948f5ee69dc414a
Sasan Jaghori <jaghori.sasan@gmail.com>
2024-10-05 10:31:04 +0200

    WIP: adds translations key table and draft search component

--- src/routes/(authenticated)/projects/[slug]/translations/+page.svelte
+++ src/routes/(authenticated)/projects/[slug]/translations/+page.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import { MainContent, MainContentHeader } from '$components/layout/main-content'
+	import SearchInput from '$components/search-input/SearchInput.svelte'
 	import type { PageData } from './$types'
+	import TranslationsKeyTable from '$components/container/translations/TranslationsKeyTable.svelte'
 	import { pageTitle } from '$lib/utils/page-title'
 
 	export let data: PageData
@@ -12,4 +14,12 @@
 
 <MainContent>
 	<MainContentHeader title="{data.project.name} - Translations"></MainContentHeader>
+
+	<div class="max-w-96">
+		<SearchInput placeholder="Search keys or translations..." />
+	</div>
+
+	<div class="mb-12 mt-4">
+		<TranslationsKeyTable />
+	</div>
 </MainContent>
